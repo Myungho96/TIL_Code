0f7474d211e3dc98234bea16502cc1a333141c8c
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-02-03 21:12:53 +0900

    [Gold V] Title: 택배 배송, Time: 520 ms, Memory: 49532 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -7,9 +8,10 @@ public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static int N, M, result, cnt;
+    public static int N, M, result;
     public static List<Node>[] list;
     public static boolean[] visited;
+    public static int[] dist;
 
     public static void main(String[] args) throws IOException {
         String[] temp;
@@ -21,13 +23,15 @@ public class Main {
             list[i] = new ArrayList<>();
         }
         visited = new boolean[N + 1];
+        dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         for (int i = 0; i < M; i++) {
             temp = br.readLine().split(" ");
             list[Integer.parseInt(temp[0])].add(new Node(Integer.parseInt(temp[1]), Integer.parseInt(temp[2])));
             list[Integer.parseInt(temp[1])].add(new Node(Integer.parseInt(temp[0]), Integer.parseInt(temp[2])));
         }
         bfs();
-        bw.write(result + "\n");
+        bw.write(dist[N] + "\n");
         bw.flush();
         bw.close();
 
@@ -35,22 +39,19 @@ public class Main {
 
     private static void bfs() {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        for (Node node : list[1]) {
-            pq.add(new Node(node.num, node.cost));
-        }
-        result = 0;
-        visited[1] = true;
+        pq.add(new Node(1, 0));
+        dist[1] = 0;
         while (!pq.isEmpty()) {
             Node node = pq.poll();
-            if (node.num == N) {
-                result = node.cost;
-                return;
-            }
             if (visited[node.num])
                 continue;
             visited[node.num] = true;
-            for (Node num : list[node.num]) {
-                pq.add(new Node(num.num, node.cost + num.cost));
+            for (Node current : list[node.num]) {
+                if (dist[current.num] > dist[node.num] + current.cost) {
+                    dist[current.num] = dist[node.num] + current.cost;
+                    pq.add(new Node(current.num, dist[current.num]));
+                }
+
             }
 
         }
