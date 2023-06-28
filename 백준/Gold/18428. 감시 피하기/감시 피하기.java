f279444d96c6f8ca300a28da21f62466f6a9971c
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-06-28 12:22:52 +0900

    [Gold V] Title: 감시 피하기, Time: 164 ms, Memory: 17444 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -1,109 +1,98 @@
+import java.lang.*;
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N;
-    static String Result;
-    static int[][] Arr;
-    static int[][] deltas = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    static List<Node> Teachers;
-    static List<Node> Blanks;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int N;
+    public static String Result = "NO";
+    public static String[][] Arr;
+    public static List<int[]> Lists;
+    public static List<int[]> Teachers;
+    public static int[][] Deltas = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public static void main(String[] args) throws IOException {
-        Teachers = new ArrayList<>();
-        Blanks = new ArrayList<>();
+        String[] temp;
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        Arr = new int[N + 1][N + 1];
-        Result = "NO";
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                String temp = st.nextToken();
-                if (temp.equals("S")) {
-                    Arr[i][j] = 2;
-                } else if (temp.equals("T")) {
-                    Arr[i][j] = 3;
-                    Teachers.add(new Node(i, j));
-                } else {
-                    Blanks.add(new Node(i, j));
+        Lists = new ArrayList<>();
+        Teachers = new ArrayList<>();
+        Arr = new String[N][N];
+        for (int i = 0; i < N; i++) {
+            temp = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                Arr[i][j] = temp[j];
+                if (Arr[i][j].equals("X")) {
+                    Lists.add(new int[]{i, j});
+                } else if (Arr[i][j].equals("T")) {
+                    Teachers.add(new int[]{i, j});
                 }
             }
         }
-        //장애물 설치
-        int BlankLen = Blanks.size();
-        label:
-        for (int i = 0; i < BlankLen; i++) {
-            Arr[Blanks.get(i).r][Blanks.get(i).c] = 1;
-            for (int j = i + 1; j < BlankLen; j++) {
-                Arr[Blanks.get(j).r][Blanks.get(j).c] = 1;
-                for (int k = j + 1; k < BlankLen; k++) {
-                    Arr[Blanks.get(k).r][Blanks.get(k).c] = 1;
-                    if (bfs() == 0) {
+
+        loop:
+        for (int i = 0; i < Lists.size(); i++) {
+            int[] num1 = Lists.get(i);
+            Arr[num1[0]][num1[1]] = "O";
+            for (int j = i + 1; j < Lists.size(); j++) {
+                int[] num2 = Lists.get(j);
+                Arr[num2[0]][num2[1]] = "O";
+                for (int k = j + 1; k < Lists.size(); k++) {
+                    int[] num3 = Lists.get(k);
+                    Arr[num3[0]][num3[1]] = "O";
+                    if (bfs()) {
                         Result = "YES";
-                        break label;
+                        break loop;
                     }
-                    Arr[Blanks.get(k).r][Blanks.get(k).c] = 0;
+
+                    Arr[num3[0]][num3[1]] = "X";
                 }
-                Arr[Blanks.get(j).r][Blanks.get(j).c] = 0;
+                Arr[num2[0]][num2[1]] = "X";
             }
-            Arr[Blanks.get(i).r][Blanks.get(i).c] = 0;
+            Arr[num1[0]][num1[1]] = "X";
         }
 
         bw.write(Result + "\n");
         bw.flush();
         bw.close();
+
+
     }
 
-    private static int bfs() {
-        Queue<Node> queue = new ArrayDeque<>();
-        int cnt = 0;
-        for (Node teacher : Teachers) {
-            queue.offer(new Node(teacher.r, teacher.c));
-//            visited[teacher.r][teacher.c] = true;
+    public static boolean bfs() {
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][N];
+
+        for (int i = 0; i < Teachers.size(); i++) {
+            int[] temp = Teachers.get(i);
+            queue.offer(new int[]{temp[0], temp[1]});
+            visited[temp[0]][temp[1]] = true;
         }
+
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            for (int i = 0; i < 4; i++) {//한 방향으로 쭉 가보기
-                Queue<Node> newQueue = new ArrayDeque<>();
-                newQueue.offer(new Node(node.r, node.c));
-                while (!newQueue.isEmpty()) {
-                    boolean[][] visited = new boolean[N + 1][N + 1];
-                    Node newNode = newQueue.poll();
-                    int cr = newNode.r + deltas[i][0];
-                    int cc = newNode.c + deltas[i][1];
-                    if (!isIn(cr, cc))
+            int[] temp = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int cr = temp[0];
+                int cc = temp[1];
+                while (true) {
+                    cr += Deltas[i][0];
+                    cc+= Deltas[i][1];
+                    if (cr >= 0 && cc >= 0 && cr < N && cc < N) {
+                        if(Arr[cr][cc].equals("S"))
+                            return false;
+                        else if(Arr[cr][cc].equals("O"))
+                            break;
+                    } else {
                         break;
-                    if (Arr[cr][cc] == 2) {
-                        cnt++;
-                        return cnt;
                     }
-                    if (!visited[cr][cc] && Arr[cr][cc] != 1) {
-                        visited[cr][cc] = true;
-                        newQueue.offer(new Node(cr, cc));
-                    }
+
                 }
-
-
             }
         }
-        return cnt;
-    }
-
-    private static boolean isIn(int r, int c) {
-        return r > 0 && c > 0 && r <= N && c <= N;
-    }
-
-    private static class Node {
-        int r, c;
-
-        public Node(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
+        return true;
     }
 
 
 }
+
