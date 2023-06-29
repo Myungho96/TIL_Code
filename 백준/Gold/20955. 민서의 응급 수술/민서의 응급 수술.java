6498df4900b0debc254e87776de53af3bd2bb05a
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-06-29 16:49:15 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 500 ms, Memory: 48336 KB -BaekjoonHub

---
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
@@ -0,0 +1,71 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int N, M;
+    public static int[] Parents;
+    public static HashSet<Integer> Sets = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        String[] temp = br.readLine().split(" ");
+        N = Integer.parseInt(temp[0]);
+        M = Integer.parseInt(temp[1]);
+        int cnt = 0;
+        make();
+        for (int i = 0; i < M; i++) {
+            temp = br.readLine().split(" ");
+//            union(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
+            if (!union(Integer.parseInt(temp[0]), Integer.parseInt(temp[1])))
+                cnt++;
+        }
+//        System.out.println(Arrays.toString(parents));
+//        for (int i : Parents) {
+//            Sets.add(i);
+//        }
+
+        for (int i = 1; i <= N; i++){
+//            if(!Sets.contains(Parents[i])){
+//                Sets.add(i);
+//            }
+            if(i==find(i)) cnt++;
+        }
+//        System.out.println(Sets);
+        bw.write( - 1 + cnt  + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void make() {
+        Parents = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            Parents[i] = i;
+        }
+    }
+
+    public static int find(int num) {
+        if (num == Parents[num])
+            return num;
+        return Parents[num] = find(Parents[num]);
+    }
+
+    public static boolean union(int num1, int num2) {
+        int temp1 = find(num1);
+        int temp2 = find(num2);
+        if (temp1 == temp2)
+            return false;
+        else if (find(temp1) <= find(temp2)) {
+            Parents[temp2] = temp1;
+        } else {
+            Parents[temp1] = temp2;
+        }
+
+        return true;
+    }
+
+
+}
+
