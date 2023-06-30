a0157245150a167190e7dffce3ef6a7332fe35d7
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-06-30 11:30:41 +0900

    [Gold V] Title: 빗물, Time: 152 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -6,45 +6,44 @@ import java.util.*;
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int H, W, Result = 0;
+    public static int[] Arr;
 
     public static void main(String[] args) throws IOException {
         String[] temp = br.readLine().split(" ");
-        int N = Integer.parseInt(temp[0]);
-        int M = Integer.parseInt(temp[1]);
-        int totalCnt = 0;
-        int[] arr = new int[M];
+        H = Integer.parseInt(temp[0]);
+        W = Integer.parseInt(temp[1]);
+        Arr = new int[W];
         temp = br.readLine().split(" ");
-        for (int i = 0; i < M; i++) {
-            arr[i] = Integer.parseInt(temp[i]);
+        for (int i = 0; i < W; i++) {
+            Arr[i] = Integer.parseInt(temp[i]);
         }
-//        System.out.println(Arrays.toString(arr));
-        //2중 포문으로 처음부터 끝까지 탐색하며 먼저 그 위치의 바로 옆이 비었는지를 체크하고, 체크하려는 높이가 첫번째보다 작은지를 체크하고(크면 물이 그냥 나가버림), 작다면 왼쪽으로 한칸씩 살펴본다.
-        //만약 살펴본 위치가 마지막인데 체크하는 높이보다 작다면 break(조건 성립안함), 그게 아니면 벽을 만날때까지 체크해서 누적한 값을 고인물로 추가한다.
-        //이후
-        for (int i = 0; i < M-1; i++) {//마지막에서는 수행할 필요가 없음.
-            for (int j = arr[i]; j > 0; j--) {
-                if(j>arr[i+1] && i+1!=M-1){//앞의 벽보다 작고 마지막이 아니라면
-                    int curCnt = 0;
-                    int pos = i;
-                    while(true){//쭉 가보면서 고이는 물 누적해보기
-                        pos++;
-                        if(pos==M-1 && j>arr[pos]){
-                            break;
-                        }else{
-                            if(j>arr[pos]){
-                                curCnt++;
-                            }else{
-                                totalCnt+=curCnt;
-                                break;
-                            }
-                        }
+        cal();
+        bw.write(Result + "\n");
+        bw.flush();
+        bw.close();
+    }
 
+    public static void cal() {
+
+        for (int i = 0; i < W - 1; i++) {
+            for (int j = 1; j <= Arr[i]; j++) {
+                int num = i+1;
+                int cnt=0;
+                while(true){
+                    if (j - Arr[num] > 0 && num != W - 1) {
+                        num++;
+                        cnt++;
+                    }else if(j - Arr[num] > 0 && num == W - 1){
+                        break;
+                    }else{
+                        Result+=cnt;
+                        break;
                     }
                 }
             }
         }
-        bw.write(totalCnt+"\n");
-        bw.flush();
-        bw.close();
     }
+
 }
+
