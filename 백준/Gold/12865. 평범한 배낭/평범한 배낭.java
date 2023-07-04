492ba4259f54c087eee72a19e02c7e4e58713ae6
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-07-04 18:00:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 212 ms, Memory: 53544 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,62 @@
-import java.util.*;
-import java.io.*;
 import java.lang.*;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N, K;
-    static int[][] dp;
-    static int[][] arr;
-    static StringTokenizer st;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int N, K;
+    public static int[][] Dp;
+    public static Node[] Products;
+
+    public static class Node {
+        int W, V;
+
+        public Node(int W, int V) {
+            this.W = W;
+            this.V = V;
+        }
+
+        @Override
+        public String toString() {
+            return W + " " + V;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        dp = new int[N + 1][K + 1];
-        arr = new int[N][2];
+        String[] temp = br.readLine().split(" ");
+        N = Integer.parseInt(temp[0]);
+        K = Integer.parseInt(temp[1]);
+        Dp = new int[N][K + 1];
+        Products = new Node[N];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            temp = br.readLine().split(" ");
+            Products[i] = new Node(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
         }
-        Arrays.sort(arr, (o1, o2) -> Integer.compare(o1[0],o2[0]));
-        solution();
-        bw.write(dp[N][K] + "\n");
+
+        cal();
+        bw.write(Dp[N - 1][K] + "\n");
         bw.flush();
         bw.close();
+
     }
 
-    private static void solution() {
-        for (int i = 1; i <= N; i++) {
+    public static void cal() {
+        //0번째는 K 넘으면 그냥 넣어주기
+        for (int i = 1; i <= K; i++)
+            if (i >= Products[0].W) {
+                Dp[0][i] = Products[0].V;
+            }
+
+        for (int i = 1; i < N; i++)
             for (int j = 1; j <= K; j++) {
-                dp[i][j] = dp[i-1][j];
-                if(j-arr[i-1][0]>=0){
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i-1][0]]+arr[i-1][1]);
+                Dp[i][j] = Dp[i - 1][j];
+                int margin = j - Products[i].W;
+                if (margin >= 0) {
+                    Dp[i][j] = Math.max(Dp[i][j], Dp[i - 1][margin] + Products[i].V);
                 }
             }
-        }
     }
 }
+
