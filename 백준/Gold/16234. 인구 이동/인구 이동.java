c58246349c0a1fc3d502362a1088c5f210627e75
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-07-04 16:49:41 +0900

    [Gold V] Title: 인구 이동, Time: 700 ms, Memory: 297620 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,90 +1,87 @@
-import java.util.*;
-import java.io.*;
 import java.lang.*;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    static String input = "3 5 10\n" +
-            "10 15 20\n" +
-            "20 30 25\n" +
-            "40 22 10";
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N, L, R, Flag, Days;
-    static int[][] arr;
-    static int[][] deltas = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static StringTokenizer st;
-    static boolean[][] visited;
-    static List<int[]> list = new LinkedList<>();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int N, L, R, Result = -1;
+    public static int[][] Arr;
+    public static int[][] Deltas = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public static boolean[][] visited;
+    public static boolean Flag;
 
     public static void main(String[] args) throws IOException {
-        //br = new BufferedReader(new StringReader(input));
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        arr = new int[N][N];
-
+        String[] temp = br.readLine().split(" ");
+        N = Integer.parseInt(temp[0]);
+        L = Integer.parseInt(temp[1]);
+        R = Integer.parseInt(temp[2]);
+        Arr = new int[N][N];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            temp = br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                Arr[i][j] = Integer.parseInt(temp[j]);
             }
         }
-        Flag = 1;
-        Days = -1;
-        while (Flag == 1) {
-            Flag = 0;
-            Days++;
+        Flag = true;
+        while (Flag) {
+            Flag = false;
+            Result++;
             visited = new boolean[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (!visited[i][j]) {
+                    if (!visited[i][j])
                         bfs(i, j);
-                    }
                 }
             }
         }
-        System.out.println(Days);
+        bw.write(Result + "\n");
+        bw.flush();
+        bw.close();
+
     }
 
-    static void bfs(int r, int c) {
-        Queue<int[]> queue = new LinkedList<>();
-        list.clear();
-        queue.offer(new int[]{r, c, arr[r][c]});
+    public static void bfs(int r, int c) {
+        Queue<Node> queue = new ArrayDeque<>();
+        Queue<Node> result = new ArrayDeque<>();
+        queue.offer(new Node(r, c));
+        int sum = Arr[r][c];
         visited[r][c] = true;
         while (!queue.isEmpty()) {
-            int[] world = queue.poll();
-            list.add(world);
-            for (int d = 0; d < deltas.length; d++) {
-                int nr = world[0] + deltas[d][0];
-                int nc = world[1] + deltas[d][1];
-                if (isIn(nr, nc)) {
-                    if (checkRange(world[2], arr[nr][nc]) && !visited[nr][nc]) {//조건 만족하면 list에 add한다.
-                        queue.offer(new int[]{nr, nc, arr[nr][nc]});
-                        visited[nr][nc] = true;
-                    }
+            Node node = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int cr = node.i + Deltas[i][0];
+                int cc = node.j + Deltas[i][1];
+                if (cr >= 0 && cc >= 0 && cr < N && cc < N && !visited[cr][cc] && Math.abs(Arr[node.i][node.j] - Arr[cr][cc]) >= L && Math.abs(Arr[node.i][node.j] - Arr[cr][cc]) <= R) {
+                    Flag = true;
+                    visited[cr][cc] = true;
+                    queue.offer(new Node(cr, cc));
+                    result.offer(new Node(cr,cc));
+                    sum+=Arr[cr][cc];
                 }
             }
         }
-        if (list.size() > 1) {//연합이 생긴 경우
-            Flag = 1;
-            int sum = 0;
-            for (int i = 0; i < list.size(); i++) {
-                sum+=list.get(i)[2];
-            }
-            int avg = sum/list.size();
-            for (int i = 0; i < list.size(); i++) {
-                arr[list.get(i)[0]][list.get(i)[1]] = avg;
+        if(result.size()>0){
+            int avg = sum/(result.size()+1);
+            Arr[r][c] = avg;
+            while(!result.isEmpty()){
+                Node node = result.poll();
+                Arr[node.i][node.j] = avg;
             }
         }
 
+
     }
 
-    static boolean isIn(int r, int c) {
-        return r >= 0 && c >= 0 && r < N && c < N;
+    public static class Node {
+        int i, j;
+
+        public Node(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
     }
 
-    static boolean checkRange(int sum1, int sum2) {
-        int sum = Math.abs(sum1 - sum2);
-        return sum >= L && sum <= R;
-    }
 }
+
