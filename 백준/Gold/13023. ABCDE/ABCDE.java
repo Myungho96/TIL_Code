2770f5a95e4a65203500b42074384d9eb2d9283f
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-07-12 13:03:37 +0900

    [Gold V] Title: ABCDE, Time: 312 ms, Memory: 20796 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,64 +1,63 @@
-import java.io.*;
 import java.lang.*;
+import java.io.*;
+import java.sql.Array;
 import java.util.*;
 
+
 public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static boolean[] visited;
+    public static int Result = 0;
 
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static StringTokenizer st;
-	public static int N, M;
-	public static int a, b, c;
-	public static List<Integer>[] list;
-	public static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        String[] temp = br.readLine().split(" ");
+        int N = Integer.parseInt(temp[0]);
+        int M = Integer.parseInt(temp[1]);
+        visited = new boolean[N];
+        List<Integer>[] lists = new List[N];
+        for (int i = 0; i < N; i++)
+            lists[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            temp = br.readLine().split(" ");
+            int a = Integer.parseInt(temp[0]);
+            int b = Integer.parseInt(temp[1]);
+            lists[a].add(b);
+            lists[b].add(a);
+        }
+        for(int i=0;i<N;i++){
+            if(Result==1)
+                break;
+            visited[i] = true;
+            dfs(0, i, lists);
+            visited[i] = false;
+        }
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		list = new List[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = new ArrayList<>();
-		}
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			list[b].add(a);
-		}
-		for (int i = 0; i < N; i++) {
-			visited = new boolean[N];
-			if (dfs(i, 0)) {
-				bw.flush();
-				bw.close();
-				return;
-			}
-		}
-		bw.write("0" + "\n");
-		bw.flush();
-		bw.close();
-		return;
+        bw.write(Result + "\n");
+        bw.flush();
+        bw.close();
 
-	}
+    }
 
-	private static boolean dfs(int index, int cnt) throws IOException {
-		if (cnt == 4) {
-			bw.write("1" + "\n");
-			return true;
-		}
-		visited[index] = true;
-		for (int data : list[index]) {
-			if (!visited[data]) {
-				boolean flag = dfs(data, cnt + 1);
-				if(flag)
-					return flag;
-				visited[data] = false;
-			}
-		}
-
-		return false;
-
-	}
+    public static void dfs(int cnt, int start, List<Integer>[] lists) {
+        int N = visited.length;
+        if (cnt == 4) {
+//            int temp = 0;
+//            for (int i = 0; i < N; i++)
+//                if (visited[i])
+//                    temp++;
+//            if (temp >= 4)
+                Result = 1;
+            return;
+        }
+        for (int num : lists[start]) {
+            if(!visited[num]){
+                visited[num]=true;
+                dfs(cnt+1,num,lists);
+                visited[num]=false;
+            }
+        }
+    }
 
 }
+
