3556d2dda6699b0102537265e4a57c87d74d56ec
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-07-27 12:51:28 +0900

    [Gold IV] Title: 문자열 폭발, Time: 760 ms, Memory: 100164 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,49 +1,43 @@
-import java.io.*;
 import java.lang.*;
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static String origin, target;
-    static Stack<Character> stack;
-    static String result;
-
-
     public static void main(String[] args) throws IOException {
-        origin = br.readLine();
-        target = br.readLine();
-        stack = new Stack<>();
-        for (int i = 0; i < origin.length(); i++) {
-            stack.push(origin.charAt(i));
+        String[] s = br.readLine().split("");
+        String[] t = br.readLine().split("");
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < s.length; i++) {
+            stack.add(s[i]);
 
-            if (stack.size() >= target.length()) {
-                boolean flag = true;
-                for (int j = 0; j < target.length(); j++) {
-                    if (stack.get(stack.size() - target.length() + j) != target.charAt(j)) {
-                        flag = false;
+            if (stack.size() >= t.length) {
+                boolean flag = false;
+                for (int j = 0; j < t.length; j++) {
+                    if(!stack.get(stack.size()-t.length+j).equals(t[j])){
+                        flag=true;
                         break;
                     }
                 }
-                if (flag) {
-                    for (int j = 0; j < target.length(); j++) {
+                if(!flag){
+                    int cnt=0;
+                    while(cnt++!=t.length)
                         stack.pop();
-                    }
                 }
             }
         }
-        result = "";
-        StringBuilder sb = new StringBuilder();
-        for (char ch : stack) {
-            sb.append(ch);
-        }
-        bw.write(sb.length() > 0 ? sb.toString() : "FRULA");
+
+        bw.write((stack.size() > 0 ? mergeStr(stack) : "FRULA") + "\n");
         bw.flush();
         bw.close();
-
+        br.close();
     }
-
+    static String mergeStr(Stack<String> stack){
+        StringBuilder sb = new StringBuilder("");
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        return sb.reverse().toString();
+    }
 }
