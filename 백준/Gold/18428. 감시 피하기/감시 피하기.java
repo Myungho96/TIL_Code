b6b7a4186db66e5716e2b86f008a98efd602c513
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-08-07 12:06:23 +0900

    [Gold V] Title: 감시 피하기, Time: 140 ms, Memory: 16200 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -2,97 +2,83 @@ import java.lang.*;
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static int N;
-    public static String Result = "NO";
-    public static String[][] Arr;
-    public static List<int[]> Lists;
-    public static List<int[]> Teachers;
-    public static int[][] Deltas = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        String[] temp;
-        N = Integer.parseInt(br.readLine());
-        Lists = new ArrayList<>();
-        Teachers = new ArrayList<>();
-        Arr = new String[N][N];
-        for (int i = 0; i < N; i++) {
-            temp = br.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                Arr[i][j] = temp[j];
-                if (Arr[i][j].equals("X")) {
-                    Lists.add(new int[]{i, j});
-                } else if (Arr[i][j].equals("T")) {
-                    Teachers.add(new int[]{i, j});
-                }
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        boolean[][] check = new boolean[n][n];
+        String result = "NO";
+        for (int i = 0; i < n; i++) {
+            char[] temp = br.readLine().toCharArray();
+            for (int j = 0; j < n; j++) {
+                if (temp[j * 2] == 'S')
+                    arr[i][j] = 1;
+                if (temp[j * 2] == 'T')
+                    arr[i][j] = 2;
             }
         }
-
+        //장애물을 check배열을 이용해 체크하고 bfs돌기
         loop:
-        for (int i = 0; i < Lists.size(); i++) {
-            int[] num1 = Lists.get(i);
-            Arr[num1[0]][num1[1]] = "O";
-            for (int j = i + 1; j < Lists.size(); j++) {
-                int[] num2 = Lists.get(j);
-                Arr[num2[0]][num2[1]] = "O";
-                for (int k = j + 1; k < Lists.size(); k++) {
-                    int[] num3 = Lists.get(k);
-                    Arr[num3[0]][num3[1]] = "O";
-                    if (bfs()) {
-                        Result = "YES";
-                        break loop;
+        for (int i = 0; i < n * n; i++) {
+            if (arr[i / n][i % n] == 0) {
+                check[i / n][i % n] = true;
+                for (int j = i + 1; j < n * n; j++) {
+                    if (arr[j / n][j % n] == 0) {
+                        check[j / n][j % n] = true;
+                        for (int k = j + 1; k < n * n; k++) {
+                            if (arr[k / n][k % n] == 0) {
+                                check[k / n][k % n] = true;
+                                if (bfs(arr, check)) {
+                                    result = "YES";
+                                    break loop;
+                                }
+                                check[k / n][k % n] = false;
+                            }
+                        }
+                        check[j / n][j % n] = false;
+
                     }
-
-                    Arr[num3[0]][num3[1]] = "X";
                 }
-                Arr[num2[0]][num2[1]] = "X";
-            }
-            Arr[num1[0]][num1[1]] = "X";
-        }
+                check[i / n][i % n] = false;
 
-        bw.write(Result + "\n");
+            }
+        }
+        bw.write(result + "\n");
         bw.flush();
         bw.close();
-
-
+        br.close();
     }
 
-    public static boolean bfs() {
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][N];
+    static int[][] deltas = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        for (int i = 0; i < Teachers.size(); i++) {
-            int[] temp = Teachers.get(i);
-            queue.offer(new int[]{temp[0], temp[1]});
-            visited[temp[0]][temp[1]] = true;
-        }
-
+    private static boolean bfs(int[][] arr, boolean[][] check) {
+        Queue<int[]> queue = new LinkedList<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (arr[i][j] == 1)
+                    queue.offer(new int[]{i, j});
         while (!queue.isEmpty()) {
-            int[] temp = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int cr = temp[0];
-                int cc = temp[1];
-                while (true) {
-                    cr += Deltas[i][0];
-                    cc+= Deltas[i][1];
-                    if (cr >= 0 && cc >= 0 && cr < N && cc < N) {
-                        if(Arr[cr][cc].equals("S"))
-                            return false;
-                        else if(Arr[cr][cc].equals("O"))
-                            break;
-                    } else {
-                        break;
-                    }
+            int[] node = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int cr = node[0] + deltas[d][0];
+                int cc = node[1] + deltas[d][1];
 
+                while (true) {
+                    if (!(cr >= 0 && cc >= 0 && cr < n && cc < n))
+                        break;
+                    if (!check[cr][cc] && arr[cr][cc] == 2)
+                        return false;
+                    else if (check[cr][cc])
+                        break;
+                    cr += deltas[d][0];
+                    cc += deltas[d][1];
                 }
             }
         }
         return true;
     }
-
-
 }
-
