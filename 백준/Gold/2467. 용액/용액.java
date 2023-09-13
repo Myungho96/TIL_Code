0fa95ebfaee6710449b310184e0d53347fadb7be
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2023-09-13 17:35:46 +0900

    [Gold V] Title: 용액, Time: 388 ms, Memory: 30164 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,36 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int Result = Integer.MAX_VALUE, s, e;
-
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        String[] temp = br.readLine().split(" ");
-        for (int j = 0; j < n; j++) {
-            arr[j] = Integer.parseInt(temp[j]);
-        }
-        int start = 0;
-        int end = n - 1;
-        while (start < end) {
-            int sum = arr[start] + arr[end];
-            if (Math.abs(sum) <= Result) {
-                Result = Math.abs(sum);
-                s = arr[start];
-                e = arr[end];
-            }
-            if (sum > 0)
+    public static void main(String args[]) throws IOException{
+      //투포인터 문제인듯
+      int n = Integer.parseInt(br.readLine());
+      int []arr = new int[n];
+      String []temp = br.readLine().split(" ");
+      for(int i=0;i<n;i++){
+          arr[i] = Integer.parseInt(temp[i]);
+      }
+      
+      int start = 0;
+      int end = n-1;
+      int min = Integer.MAX_VALUE;
+      int minNum1=0, minNum2=0;
+      
+      while(start<end){
+          if(min>=Math.abs(arr[start]+arr[end])){
+              min = Math.abs(arr[start]+arr[end]);
+              minNum1= start;
+              minNum2 = end;
+          }
+          if(arr[start]+arr[end]>0)
                 end--;
-            else
+              else
                 start++;
-        }
-
-        bw.write(s + " " + e + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+      }
+  
+      bw.write(arr[minNum1] + " "+arr[minNum2]+"\n");
+      bw.flush();
+      bw.close();
     }
 }
